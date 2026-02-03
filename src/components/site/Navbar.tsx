8d394c127928b2a515b90b327807314573c3bb20
M <manuelpamm@gmail.com>
2026-02-03 04:30:22 +0100

    bg 2

--- src/components/site/Navbar.tsx
+++ src/components/site/Navbar.tsx
@@ -168,7 +168,7 @@ export default function Navbar() {
             paddingBottom: scrolled ? '1rem' : '1.5rem'
           }}
           transition={{ duration: 0.4, ease: [0.4, 0, 0.2, 1] as const }}
-          className="mx-auto max-w-[96rem] pl-4 pr-[max(0.5rem,env(safe-area-inset-right,0px))] sm:pl-6 lg:px-8"
+          className="mx-auto max-w-[96rem] pl-4 pr-[max(1rem,env(safe-area-inset-right,0px))] sm:pl-6 lg:px-8"
         >
           <motion.div
             animate={{
@@ -241,7 +241,7 @@ export default function Navbar() {
         className="lg:hidden fixed z-50"
         style={{
           top: 'calc(1.5rem + env(safe-area-inset-top, 0px))',
-          right: 'max(0.5rem, env(safe-area-inset-right, 0px))',
+          right: 'max(1rem, env(safe-area-inset-right, 0px))',
         }}
       >
         <button
